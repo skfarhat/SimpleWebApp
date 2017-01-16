ce64bafdab389a0ea16d2d5b8ffebf2ce82b5101
Sami Farhat <sami.kfarhat@gmail.com>
2017-01-16 15:36:04 +0000

    update tests

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -15,12 +15,28 @@ public class QueryProcessorTest {
     }
 
     @Test
+    public void isNotCaseSensitive() throws Exception {
+        assertThat(queryProcessor.process("ivan"), containsString("teacher"));
+    }
+
+    @Test
+    public void knowsAboutDarwin() throws Exception {
+        assertThat(queryProcessor.process("darwin"), containsString("cutie"));
+    }
+
+    @Test
     public void knowsAboutShakespeare() throws Exception {
         assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
     }
 
+
     @Test
-    public void isNotCaseSensitive() throws Exception {
-        assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
+    public void knowsAboutSami() throws Exception {
+        assertThat(queryProcessor.process("sami"), containsString("student"));
+    }
+
+    @Test
+    public void knowsAboutCameron() throws Exception {
+        assertThat(queryProcessor.process("cameron"), containsString("referendum"));
     }
 }
