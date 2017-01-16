b247f1d22fc3ea967d0723d5416d1bea220db29f
Bobby Raju <bobby.raju@swisscom.com>
2017-01-16 15:40:17 +0000

    lol

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,23 +2,6 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        }
-        else if (query.toLowerCase().contains("darwin")) {
-            return "Darwin is a cutie pie";
-        }
-        else if (query.toLowerCase().contains("ivan")) {
-            return "APE teacher";
-        }
-        else if (query.toLowerCase().contains("sami")) {
-            return "APE student";
-        }
-        else if (query.toLowerCase().contains("cameron")) {
-            return "He resigned after losing votes in the referendum.......";
-        }
-        return "";
+        return "sami is a twat";
     }
 }
